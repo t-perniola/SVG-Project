3914934a0e9c69fe59cf3339e2baba80188649b1
t-perniola <t.perniola1@studenti.uniba.it>
2022-06-15 16:50:00 +0200

    modificato script Mentore
    Ora la schermata della UI si chiude alla seconda pressione del tasto M, facendo tornare il mentore nello stato Idle.

--- SVG/Assets/Scripts/MentoreScripted.cs
+++ SVG/Assets/Scripts/MentoreScripted.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MentoreScripted : MonoBehaviour{   
-    
+public class MentoreScripted : MonoBehaviour
+{
+
     private Vector3 movement;
-    private bool isInFrontOf = false;  
+    private bool isInFrontOf = false;
     private bool isMpressed = false;
     private bool isCreated = false;
     private Vector3 direction;
@@ -13,57 +14,76 @@ public class MentoreScripted : MonoBehaviour{
     private float angle;
     private GameObject windowUI;
     public float checkAngle;
-    public Transform target;       
+    public Transform target;
     public GameObject UI;
-   
-    public  Animator mAnimator;
+
+    public Animator mAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test started...");
+        //Debug.Log("Test started...");
     }
-
-    // Update is called once per frame
+    
     void Update()
-    {   
-        if(Input.GetKeyDown(KeyCode.M)) {
+    { // Update is called once per frame
+        if (Input.GetKeyDown(KeyCode.M))
+        {
             isMpressed = true;
             mKeyCounter++;
             mAnimator.SetTrigger("PlayerCall");
-            Debug.Log("M key pressed! Times: " + mKeyCounter); //M if we want to invoke the Mentore            
-        }                
+        }
     }
 
-    void FixedUpdate() { //FixedUpdate is faster than Update while working with physics
-       
+    void FixedUpdate()
+    { //FixedUpdate is faster than Update while working with physics
+
+        if (isMpressed)
+        {
+            if (mKeyCounter == 1) //if we have pressed M once, so we want to visualize UI
+            { 
+                if (CompareAngle())
+                    InstantiateUI();
+            }
+            else if (mKeyCounter == 2) //if we have pressed M twice, so we want to close UI            
+                DestroyUI();            
+        }
+    }
+
+    void InstantiateUI()
+    {
+        Vector3 playerPos = target.position;
+        Vector3 playerDirection = target.forward;
+        float spawnDistance = 1;
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+        spawnPos.y += 1;
+
+        if (!isCreated && isInFrontOf)
+        { //instantiate only once at the right time
+            windowUI = Instantiate(UI, spawnPos, target.rotation, target);
+            isCreated = true;
+        }
+    }
+
+    void DestroyUI()
+    {
+        mKeyCounter = 0;
+        Destroy(windowUI);
+        isCreated = false;
+        isInFrontOf = false;
+    }
+
+
+    bool CompareAngle()
+    {
         direction = transform.position - target.position; //distance between this object and his target (a connecting line)            
         angle = Vector3.Angle(transform.up, direction);  //calculates the angle between the line that connect the two objects
-                                                                // and the z-axis of this object       
-        if(isMpressed) {            
-            if(mKeyCounter == 0) {        
-                if (angle < checkAngle ) {  //if the angle calc. is lower than a prefixed number...   
-                    isInFrontOf = true;
-                    //Debug.Log("Mentore is in front of the Player!");
-                }                  
-
-                Vector3 playerPos = target.position;
-                Vector3 playerDirection = target.forward;            
-                float spawnDistance = 1; 
-                Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
-                spawnPos.y += 1;            
-
-                if(!isCreated && isInFrontOf) { //instantiate only once at the right time
-                    windowUI = Instantiate(UI, spawnPos, target.rotation, target);                
-                    isCreated = true;
-                }   
-                
-            } else {                
-                mKeyCounter = 0;
-                Destroy(windowUI);
-            }       
-
+                                                         // and the z-axis of this object
+        if (angle < checkAngle)
+        {  //if the angle calc. is lower than a prefixed number...   
+            isInFrontOf = true;
         }
-    
+        return isInFrontOf;
     }
+
 }
