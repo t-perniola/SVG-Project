d0c6e10012e7712fbec5cd26bb8df4847e3efafd
Giacomo Signorile <79905812+GiacomoSignorile@users.noreply.github.com>
2022-06-16 16:54:35 +0200

    Aggiunta UI Sci-Fi

---
+++ SVG/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking 
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackEange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+            //Vedi se nel range di attacco o visione
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position,sightRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();   
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if(!walkPointSet) SearchWalkPoint();
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange,walkPointRange);
+        float randomX = Random.Range(-walkPointRange,walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        //f (Physics.Raycast(walkPoint, -transform.up, ))
+    }
+
+    private void ChasePlayer()
+    {
+
+    }
+
+    private void AttackPlayer()
+    {
+
+    }
+
+}
