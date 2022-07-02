672a9b0ccdd51427c9c82a9b4a3bf0035305e1b3
Ester <estermolinari135@gmail.com>
2022-07-02 14:36:32 +0200

    Non mi ricordo cosa ho fatto, ma ho fatto tanto

---
+++ SVG/Assets/Scripts/InteractionUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractionUI : MonoBehaviour
+{
+    public TextMeshProUGUI Name;
+
+    public TextMeshProUGUI Text;
+}
