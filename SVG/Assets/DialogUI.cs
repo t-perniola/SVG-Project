e955dfb39334e2e11644b5c14518cec6ef2fc7da
Ester <estermolinari135@gmail.com>
2022-07-02 11:42:20 +0200

    Aggiunto TMP + varie UI + script

---
+++ SVG/Assets/DialogUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogUI : MonoBehaviour
+{
+    public Button yesBtn ;
+    public Button noBtn;
+    public TextMeshProUGUI dialogUI;
+}
