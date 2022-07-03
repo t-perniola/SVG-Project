9da736bb6156be7e045d8e2746d4b58bf589701f
Luca Zeverino <61737957+lucazeverino@users.noreply.github.com>
2022-07-03 21:27:21 +0200

    Terminata Scena LoseScreen

--- SVG/Assets/Scripts/RestartGame.cs
+++ SVG/Assets/Scripts/RestartGame.cs
@@ -5,16 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 
     public void LoadGame()
